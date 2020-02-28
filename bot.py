ea2dee77251655ebc552553d121d69b9a0399551
Renne Rocha <renne@rennerocha.com>
2020-02-27 23:53:14 -0300

    Add pizza command

--- bot.py
+++ bot.py
@@ -1,11 +1,19 @@
 import logging
+import math
 import random
 from datetime import datetime
 
 import requests
 from dynaconf import settings
 from ics import Calendar
-from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import (
+    CommandHandler,
+    ConversationHandler,
+    Filters,
+    MessageHandler,
+    Updater,
+)
 from tinydb import Query, TinyDB
 
 logging.basicConfig(
@@ -117,10 +125,6 @@ def grana(update, context):
     )
 
 
-def pizza(update, context):
-    context.bot.send_message(update.message.chat_id, text="Quero... \U0001F355")
-
-
 def historico(update, context):
     context.bot.send_message(update.message.chat_id, text="\U0001F914")
 
@@ -197,6 +201,39 @@ def update_lhc_status(context):
         )
 
 
+def pizza(update, context):
+    numeric_keyboard = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]]
+    update.message.reply_text(
+        "Quantas pessoas vão querer pizza \U0001F355 ???",
+        reply_markup=ReplyKeyboardMarkup(numeric_keyboard, one_time_keyboard=True),
+    )
+    return 1
+
+
+def pizza_calculator(update, context):
+    text = update.message.text
+    context.user_data["choice"] = text
+
+    no_pessoas = int(text)
+    if no_pessoas < 0:
+        update.message.reply_text(
+            "Número negativo de pizzas? Não viramos uma pizzaria."
+        )
+    elif no_pessoas == 0:
+        update.message.reply_text("Para nenhuma pessoa, é melhor nem comprar pizza.")
+    elif 1 <= no_pessoas <= 100:
+        no_pizzas = math.ceil(3 * (no_pessoas + 1) / 8)
+        update.message.reply_text(
+            f"Para {no_pessoas} pessoas, compre {no_pizzas} pizzas de 8 \U0001F355."
+        )
+    elif no_pessoas > 100:
+        update.message.reply_text(
+            "Mais que 100 pessoas no LHC? Isso vai dar overflow nos meus cálculos, se vira aí."
+        )
+
+    return -1
+
+
 def init_bot():
     updater = Updater(settings.TELEGRAM_API_TOKEN, use_context=True)
     updater.job_queue.run_repeating(update_lhc_status, interval=60, first=0)
@@ -210,8 +247,16 @@ def init_bot():
     dispatcher.add_handler(CommandHandler("grana", grana))
     dispatcher.add_handler(CommandHandler("batima", batima))
     dispatcher.add_handler(CommandHandler("boom", boom))
-    dispatcher.add_handler(CommandHandler("pizza", pizza))
     dispatcher.add_handler(CommandHandler("historico", historico))
+
+    pizza_conversation_handler = ConversationHandler(
+        entry_points=[CommandHandler("pizza", pizza)],
+        states={1: [MessageHandler(Filters.regex(r"^\d+$"), pizza_calculator)]},
+        fallbacks=[],
+    )
+    dispatcher.add_handler(pizza_conversation_handler)
+
+    # This dispatcher must be the last included
     dispatcher.add_handler(MessageHandler(Filters.text, non_commands))
 
     updater.start_polling()

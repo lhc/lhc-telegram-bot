e65b714506e6e819f48eac168e0b94975de36489
Renne Rocha <renne@rennerocha.com>
2023-12-22 20:10:28 -0300

    Check infrastructure status from montastic.io RSS file

--- src/joker/commands/status.py
+++ src/joker/commands/status.py
@@ -1,3 +1,9 @@
+import httpx
+import parsel
+
+from joker import settings
+
+
 async def status(update, context):
     await context.bot.send_message(
         update.message.chat_id,
@@ -9,4 +15,15 @@ async def quem(update, context):
     await context.bot.send_message(
         update.message.chat_id,
         text="Eu não faço a menor ideia quem está no LHC agora.",
+    )
+
+
+async def status_infra(update, context):
+    response = httpx.get(settings.MONTASTIC_RSS_URL)
+    rss = parsel.Selector(response.text)
+    status = rss.css("item title").getall()
+    status_msg = "\n".join(status)
+    await context.bot.send_message(
+        update.message.chat_id,
+        text=f"Status da infraestrutura do LHC:\n\n{status_msg}",
     )

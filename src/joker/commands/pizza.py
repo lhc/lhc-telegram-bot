b8955b43e68a0c35093c05111a4597fbd27f97d7
L. Pereira <l@tia.mat.br>
2025-09-11 19:12:08 -0700

    Melhora /pizza

--- src/joker/commands/pizza.py
+++ src/joker/commands/pizza.py
@@ -20,17 +20,21 @@ async def pizza_calculator(update, context):
     try:
         no_pessoas = int(text)
     except ValueError:
-        reply_message = "Eu só entendo uma quantidade numérica de pessoas."
+        reply_message = "Eu só entendo uma quantidade numérica e inteira de pessoas."
     else:
         if no_pessoas < 0:
             reply_message = "Número negativo de pizzas? Não viramos uma pizzaria."
         elif no_pessoas == 0:
             reply_message = "Para nenhuma pessoa, é melhor nem comprar pizza."
         elif 1 <= no_pessoas <= 100:
-            reply_message = f"Com {no_pessoas} pessoas comendo 3 pedaços\U0001F355 cada:\n"
-            for tamanho in (8, 16, 22):
-                no_pizzas = math.ceil(3 * (no_pessoas + 1) / tamanho)
-                reply_message += f"• {no_pizzas} pizzas de {tamanho} pedaços\n"
+            for pedacos in (1, 2, 3):
+                reply_message = f"Com {no_pessoas} pessoa{'s' if no_pessoas > 1 else ''} comendo {pedacos} pedaço{'s' if pedacos > 1 else ''}\U0001F355 cada:\n"
+                ultimo_calculo = -1
+                for tamanho in (8, 16, 22):
+                    no_pizzas = math.ceil(pedacos * (no_pessoas + 1) / tamanho)
+                    if no_pizzas != ultimo_calculo:
+                        reply_message += f"• {no_pizzas} pizzas de {tamanho} pedaços\n"
+                        ultimo_calculo = no_pizzas
             reply_message += "Essa quantidade é calculada para sobrar um pouco"
         elif no_pessoas > 100:
             reply_message = "Mais que 100 pessoas no LHC? Isso vai dar overflow nos meus cálculos, se vira aí."
